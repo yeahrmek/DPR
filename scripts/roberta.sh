a7d27f63ef8c2d0b35fbc3a4203b5b53972bc525
Yermek Kapushev <kapushev@gmail.com>
2023-05-31 10:29:28 +0300

    Prepared data without `<None>` samples

--- scripts/roberta.sh
+++ scripts/roberta.sh
@@ -1,6 +1,6 @@
-python -m torch.distributed.launch --nproc_per_node=8 train_dense_encoder.py \
+python -m torch.distributed.launch --nproc_per_node=2 train_dense_encoder.py \
     encoder=[hf_roberta] \
-    train_datasets=[lean_questions_train] \
-    dev_datasets=[lean_questions_dev] \
+    train_datasets=[lean_questions_no_none_train] \
+    dev_datasets=[lean_questions_no_none_dev] \
     train=biencoder_lean_questions \
-    output_dir=outputs
+    output_dir=outputs_no_none
